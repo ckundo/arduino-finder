d4ad1bea3e1c06196c1f0adb302899b8761784ee
Cameron Cundiff <ckundo@gmail.com>
2012-03-29 12:01:23 -0400

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+ArduinoFinder::Application.config.secret_token = '870249b233d6a5a4b5a7f231e3fd9d44eb3bf0d90c0fba18ad2d35737f181dc8d303fe1de7e22b18dc45ea58a3d698d92c804a98c33187aa12d0c97b16b07d4c'
