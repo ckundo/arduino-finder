d779f7c02b15b5c2fa923595f88d8cbe9db66d38
Cameron Cundiff <ckundo@gmail.com>
2012-04-02 17:24:21 -0400

    added maps

--- app/controllers/stores_controller.rb
+++ app/controllers/stores_controller.rb
@@ -1,7 +1,18 @@
 require 'radar'
 class StoresController < ApplicationController
   def index
-    locations = Radar::Shack.scan
-    render json: locations
+    loc = request.location
+    default = loc.postal_code.empty? ? '10012' : loc.postal_code
+    zip = params['zip'] || default
+    coords = Geocoder.search(zip).first.coordinates #.first.data.fetch('geometry').fetch('location') if geo.empty?
+    @lat = coords[0]
+    @lng = coords[1]
+    Radar::Shack.scan(zip)
+    @locations = Store.near(zip)
+
+    respond_to do |format|
+      format.html # index.html.erb
+      format.json { render json: @locations }
+    end 
   end
 end
--- lib/radar.rb
+++ lib/radar.rb
@@ -2,14 +2,15 @@ module Radar
   class Shack
     @base_uri = 'http://www.radioshack.com'
     
-    def self.scan(zip = 11231, product_id = 12268262)
+    def self.scan(zip = 10012, product_id = 12268262)
+      
       query = {:productId => product_id, :zip => zip}
       resp = HTTParty.get("#{@base_uri}/product/inStoreAvail.jsp", :query => query, :format => :html)
       doc = Nokogiri::HTML(resp)
       
       locations = []
       doc.css('a').each do |link|
-        href = link['href']
+        href = link['href'] || ''
         if href.include?('/map/index.jsp?')
           name = href.match(/locName=.*&/)[0].split('=')[1].split('&')[0]
           latitude = href.match(/latitude=.*&/)[0].split('=')[1].split('&')[0]
